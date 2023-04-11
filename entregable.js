b83e8addde9dd317c7fe6382a7477cf72f4d0b1c
Hernán Octavio Rojas <104470156+Spuknan@users.noreply.github.com>
2023-04-11 12:26:35 -0300

    Entrega actualizada y corregida

--- entregable.js
+++ entregable.js
@@ -1,16 +1,22 @@
 console.clear();
 
 class ProductManager {
-  constructor(products) {
-    this.products = products || [];
+  constructor() {
+    this.products = [];
     this.#lastProductId = 0;
   }
 
   #lastProductId;
 
   addProduct(title, description, price, thumbnail, code, stock) {
+    if (!title || !description || !price || !thumbnail || !code || !stock) {
+      console.error("Falta completar un campo.")
+      return false;
+    }
+  
     for (let i = 0; i < this.products.length; i++) {
       if (this.products[i].code === code) {
+        console.error(`Ya existe otro producto con el código ${code}, por favor, ingrese uno nuevo.`)
         return false;
       }
     }
@@ -24,8 +30,9 @@ class ProductManager {
       code,
       stock
     };
-
+  
     this.products.push(newProduct);
+    console.log(`Producto ${newProduct.id} (${newProduct.title}) agregado con exito.`);
     return true;
   }
 
@@ -36,27 +43,34 @@ class ProductManager {
   getProductById(id) {
     const product = this.products.find((p) => p.id === id);
     if (product) {
+      console.log(`Producto encontrado: ${product.id} - ${product.title}.`)
       return product;
     }
+    console.log('Not Found');
+    return undefined;
   }
 }
 
-// TESTING
-const productManager = new ProductManager(); // Inicializando productManager vacio.
 
-// Test 1 --> El array debe existir y estar vacío.
-console.log("Test 1 --> Verificar si el array existe y se encuentra vacío.");
 
-const products = productManager.getProducts();
-if (products.length > 0) {
-  console.error("TEST 1 FALLIDO");
-} else {
-  console.warn("TEST 1 EXITOSO");
-}
 
-// Test 2 --> Se debe agregar correctamente el producto.
-console.log("Test 2 --> Verificar el funcionamiento de addProduct.");
-productManager.addProduct(
+////////////////////////////////////
+////////////// TESTING//////////////
+////////////////////////////////////
+
+// Se creará una instancia de la clase “ProductManager”
+const pm = new ProductManager();
+
+// Se llamará “getProducts” recién creada la instancia, debe devolver un arreglo vacío []
+console.info("TEST 1 --> getProducts debe devolver un arreglo vacío.");
+const test1 = pm.getProducts();
+console.log(test1);
+test1.length ? console.error("TEST 1 FALLIDO") : console.warn("TEST 1 EXITOSO"), true;
+
+// Se llamará al método “addProduct”
+// El objeto debe agregarse satisfactoriamente con un id generado automáticamente SIN REPETIRSE
+console.info("TEST 2 --> addProduct debe agregar un nuevo producto.");
+const test2 = pm.addProduct(
   "producto prueba",
   "Este es un producto prueba",
   200,
@@ -64,45 +78,35 @@ productManager.addProduct(
   "abc123",
   25
 );
+console.log(test2);
+test2 ? console.warn("TEST 2 EXITOSO") : console.error("TEST 2 FALLIDO");
 
-if (productManager.products.length > 0) {
-  console.warn("TEST 2 EXITOSO");
-} else {
-  console.error("TEST 2 FALLIDO");
-}
+// Se llamará el método “getProducts” nuevamente, esta vez debe aparecer el producto recién agregado
+console.info("TEST 3 -->  getProducts debe devolver el producto añadido.");
+const test3 = pm.getProducts();
+console.log(test3);
+test3.length ? console.warn("TEST 3 EXITOSO") : console.error("TEST 3 FALLIDO");
 
-// Test 3 --> Mostrar el producto agregado con getProducts().
-console.log("Test 3 --> Se debe devolver el array con el producto agregado.");
-console.log(productManager.getProducts());
-
-if (productManager.products.length > 0) {
-  console.warn("TEST 3 EXITOSO");
-} else {
-  console.error("TEST 3 FALLIDO");
-}
-
-// Test 4 --> Al intentar agregar un producto con el mismo code debe verse un mensaje de error.
-console.log("Test 4 --> Verificar que no se permita crear dos objetos con el mismo code");
-productManager.addProduct(
-  "producto prueba2",
-  "Este es un producto prueba2",
-  500,
+// Se llamará al método “addProduct” con los mismos campos de arriba, debe arrojar un error porque el código estará repetido.
+console.info("TEST 4 --> addProduct (usando un código repetido) debe devolver un error.");
+const test4 = pm.addProduct(
+  "producto prueba",
+  "Este es un producto prueba",
+  200,
   "Sin imagen",
   "abc123",
-  40
+  25
 );
+console.log(test4);
+test4 ? console.error("TEST 4 FALLIDO") : console.warn("TEST 4 EXITOSO");
 
-if (productManager.products.length > 0) {
-  console.warn("TEST 3 EXITOSO");
-} else if (productManager.products.length > 1) {
-  console.error("TEST 3 FALLIDO");
-}
+// Se evaluará que getProductById devuelva error si no encuentra el producto o el producto en caso de encontrarlo
+console.info("TEST 5 --> getProductsById con un ID existente debe devolver el producto.");
+const test5 = pm.getProductById(1);
 
+console.info('TEST 6 --> getProductById con un ID inexistente debe devolver "Not Found".');
+const test6 = pm.getProductById(25);
 
-// Test 5 --> Se deben probar las dos instancias de getProductById. Tanto si encuentra un producto como si no encuentra nada.
-console.log("Test 5 --> Debe mostrarse un error y luego un mensaje de confirmación.");
-if (!productManager.getProductById(0) && productManager.getProductById(1)) {
-  console.warn("TEST 5 EXITOSO");
-} else {
-  console.error("TEST 5 FALLIDO");
-};
+console.log(test5);
+test5 ? console.warn("TEST 5 EXITOSO") : console.error("TEST 5 FALLIDO");
+test6 ? console.error("TEST 6 FALLIDO") : console.warn("TEST 6 EXITOSO");
